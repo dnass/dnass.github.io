ac97f30353cc2658d74a4bd25c92cc4ec216b7ad
Daniel Nass <dnlnss@gmail.com>
2024-10-13 11:13:26 -0400

    Design and layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://dnass.github.io',
   integrations: [tailwind()],
+  compressHTML: false,
 });
---
+++ src/components/footer.md
@@ -0,0 +1,1 @@
+[Built by Daniel Nass](https://github.com/dnass/dnass.github.io) in 2024, with Astro, Tailwind CSS, and GitHub Pages.
