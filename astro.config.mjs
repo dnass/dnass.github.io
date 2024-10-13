295db8c0b28ca33177137d9040b3e7f514cb81c6
Daniel Nass <dnlnss@gmail.com>
2024-10-13 11:25:05 -0400

    Domain setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dnass.github.io',
+  site: 'https://danielnass.net',
   integrations: [tailwind()],
   compressHTML: false,
 });
